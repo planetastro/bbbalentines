c1b4aab8e47e405e3b757189b20a798ec9ebd13d
William Setiawan <willc0d3r@gmail.com>
2026-02-14 03:40:01 +0800

    Fix GitHub Actions workflow

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
       strict: true,
     }),
     paths: {
-      base: process.argv.includes('dev') ? '' : 'bbbalentines'
+      base: process.env.NODE_ENV === 'production' ? (process.env.BASE_PATH || '/bbbalentines') : ''
     }
   }
 };
